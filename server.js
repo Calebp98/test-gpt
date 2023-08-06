5ef3f6d8db9e7d16332fd7a648b8b7cda998f753
caleb parikh <calebparikh@calebs-MacBook-Pro-2.local>
2023-08-05 17:01:52 -0700

    added favicon

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(express.static('public'));
 app.use('/static', express.static(path.join(__dirname, 'static')))
 
 
+
 const huggingfaceToken = process.env.HUGGINGFACE_TOKEN;
 
 const API_URL_BASE = "https://api-inference.huggingface.co/models/";
@@ -135,6 +136,9 @@ async function generateHuggingfaceText(model, prompt) {
         throw error;  // propagate the error so the caller can handle it
     }
 }
+
+app.get('/favicon.ico', (req, res) => res.sendStatus(204));
+
 
 // app.listen(3000, () => console.log('Server started on port 3000'));
 module.exports = serverless(app);
